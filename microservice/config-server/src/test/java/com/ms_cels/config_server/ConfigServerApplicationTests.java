f686d62547fdb87e1d7a4390bb27a9d77ba57614
Carlos Lott <celotts@gmail.com>
2025-03-04 18:43:24 -0600

    (feature): add config

---
+++ microservice/config-server/src/test/java/com/ms_cels/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_cels.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
